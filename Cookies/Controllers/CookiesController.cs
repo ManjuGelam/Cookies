f65e6c0f9b1529e9a67cb2db3abf53eea0e3bfa7
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 18:04:01 +0530

    Add project files.

---
+++ Cookies/Controllers/CookiesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cookies.Controllers
+{
+    public class CookiesController : Controller
+    {
+        public ViewResult Index1(int? Id, string Name, double? Price)
+        {
+            HttpCookie cookie = new HttpCookie("ProductCookie");
+            cookie["Id"] = Id.ToString();
+            cookie["Name"] = Name;
+            cookie["Price"] = Price.ToString();
+            cookie.Expires = DateTime.Now.AddDays(3);
+            Response.Cookies.Add(cookie);
+            return View();
+        }
+        public ViewResult Index2()
+        {
+            return View();
+        }
+
+
+    }
+}
